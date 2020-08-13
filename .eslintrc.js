2bbe311b9cdb4df381fae5d123bcaeacb8c1bf93
Valeriy Checha <valeriy.checha@onix-systems.com>
2020-08-13 15:57:51 +0300

    provide action version 0.1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,30 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es2020: true,
-    node: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parserOptions: {
-    ecmaVersion: 11,
-  },
-  rules: {
-  },
+    env: {
+        commonjs: true,
+        es2020: true,
+        node: true,
+    },
+    extends: [
+        'airbnb-base',
+    ],
+    parserOptions: {
+        ecmaVersion: 11,
+    },
+    rules: {
+        'max-len': [
+            'error',
+            150,
+        ],
+        'import/no-unresolved': 'off',
+        'no-useless-constructor': 'off',
+        'no-unused-vars': 'off',
+        'import/extensions': 'off',
+        'no-empty-function': 'off',
+        'no-underscore-dangle': 'off',
+        indent: [
+            'error',
+            4,
+        ],
+        'arrow-body-style': 'off',
+    },
 };
